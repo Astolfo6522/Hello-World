2f22ac29c6863ed89072b5ddea8c50b459984f02
Astolfo <69761239+Astolfo6522@users.noreply.github.com>
2020-11-03 12:45:46 +0700

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+   string name;
+   cout << "\033[32mHello World!\033[0m\n";
+   cout << "\n\033[34mHello, What is your name?\n> ";
+   getline(cin, name);
+   cout << "\n\033[35mHello, \033[92m" << name << "\033[0m!";
    return 0;
 }
