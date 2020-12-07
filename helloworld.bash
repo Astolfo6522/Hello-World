a8197d97c3b268d5db2756b502ea467273d5e4f3
Astolfo <69761239+Astolfo6522@users.noreply.github.com>
2020-12-07 20:16:46 +0700

    Create helloworld.bash
    Bash Script.

---
+++ helloworld.bash
@@ -0,0 +1,9 @@
+#/usr/bin/bash
+#/data/data/com.termux/files/usr/bin/bash
+#This can be executing by adding "!" on first 2 above this comment
+#Neither you are Termux User or Linux User or any UNIX FSH
+
+word="Hello World"
+color="\033[32m"
+endc="\033[0m"
+echo "$color $word $endc"
